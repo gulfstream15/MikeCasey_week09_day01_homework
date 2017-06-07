71640421ab9c3a5b5f267b0037ade75c3b3fdc9d
User <user@Users-MBP.home>
2017-06-07 21:31:54 +0100

    Added initial java code within android - will complete later on

---
+++ BucketSort/app/src/main/java/com/codeclan/example/bucketsort/BucketSort.java
@@ -0,0 +1,8 @@
+package com.codeclan.example.bucketsort;
+
+/**
+ * Created by user on 07/06/2017.
+ */
+
+public class BucketSort {
+}
---
+++ BucketSort/app/src/test/java/com/codeclan/example/bucketsort/BucketSortTest.java
@@ -0,0 +1,25 @@
+package com.codeclan.example.bucketsort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by user on 07/06/2017.
+ */
+
+public class BucketSortTest {
+
+    @Before
+    public void before() {
+        BucketSort bucketSort = new BucketSort ();
+    }
+
+    @Test
+    public void startArrayIsNotEmpty() {
+
+    }
+
+
+
+
+}
